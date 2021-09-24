83d27265ad81f68e8457d6fccaccc13a852bb79c
StefanRacic <86464790+racicstefan@users.noreply.github.com>
2021-09-24 16:50:13 +0200

    GithubFinder finished

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
 		user: {},
 		loading: false,
 		alert: null,
+		repos: [],
 	};
 
 	searchUsers = async (text) => {
@@ -41,6 +42,18 @@ class App extends Component {
 		this.setState({ user: res.data, loading: false });
 	};
 
+	// Get users repos
+	getUserRepos = async (username) => {
+		this.setState({ loading: true });
+
+		const res = await axios.get(
+			`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+			&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+
+		this.setState({ repos: res.data, loading: false });
+	};
+
 	clearUsers = () => this.setState({ users: [], loading: false });
 
 	setAlert = (message, type) => {
@@ -51,7 +64,7 @@ class App extends Component {
 	};
 
 	render() {
-		const { loading, users, user, alert } = this.state;
+		const { loading, users, repos, user, alert } = this.state;
 		return (
 			<Router>
 				<div className='App'>
@@ -82,7 +95,9 @@ class App extends Component {
 									<User
 										{...props}
 										getUser={this.getUser}
+										getUserRepos={this.getUserRepos}
 										user={user}
+										repos={repos}
 										loading={loading}
 									/>
 								)}
